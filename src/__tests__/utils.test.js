8df00ecb233024ba544823e5311d7141543755ac
Duke <dukemorise33@gmail.com>
2023-09-30 19:28:32 -0700

    completed tdd unit test lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,52 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+    it('expects isPalindrome to comeback as true when argument is a palindrome', () => {
+        const palindrome1 = 'racecar';
+        const palindrome2 = 'aa';
+        const palindrome3 = 'leeel';
+        const palindrome4 = 'aaa';
+        const palindrome5 = 'Hector';
+
+        const result1 = isPalindrome(palindrome1);
+        const result2 = isPalindrome(palindrome2);
+        const result3 = isPalindrome(palindrome3);
+        const result4 = isPalindrome(palindrome4);
+        const result5 = isPalindrome(palindrome5);
+
+        expect(result1).toBe(true);
+        expect(result2).toBe(true);
+        expect(result3).toBe(true);
+        expect(result4).toBe(true);
+        expect(result5).toBe(false);
+    }),
+    it('expects isPalindrome to identify lower and uppercased words', ()=>{
+        const palindrome1 = 'Heceh';
+        const palindrome2 = 'Aaa';
+        const palindrome3 = 'AaA';
+
+        const result1 = isPalindrome(palindrome1);
+        const result2 = isPalindrome(palindrome2);
+        const result3 = isPalindrome(palindrome3);
+
+        expect(result1).toBe(false);
+        expect(result2).toBe(false);
+        expect(result3).toBe(true);
+    }),
+    it('isPalindrome is expected to return false when argument is an empty string' ,()=>{
+        const palindrome = '';
+
+        const result = isPalindrome(palindrome);
+
+        expect(result).toBe(false);
+    }),
+    it('isPalindrome will return an error if the argument is not a string', ()=>{
+        const palindrome = 123;
+
+        expect(()=>{
+            isPalindrome(palindrome);
+        }).toThrow(new Error('argument must be a string'));
+    })
+
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
 // Your code here
+export function isPalindrome(word) {
+    if (!word) {
+        return false;
+    }
+    else if (typeof word !== 'string') {
+        throw new Error('argument must be a string');
+    }
+
+    //word is going to be compared...
+    const wordArray = word.split('');
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join('');
+
+    if (word === reversedWord) {
+        return true;
+    }
+
+    return false;
+}
